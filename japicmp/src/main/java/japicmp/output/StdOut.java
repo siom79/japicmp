e1adbc2462ee38d16c78b6642d097f70c5608e20
eldurloki <eldurloki@users.sourceforge.net>
2015-01-12 19:14:33 +0100

    Extracted CLI output generators to new type
    Task: #26

---
+++ japicmp/src/main/java/japicmp/output/StdOut.java
@@ -0,0 +1,22 @@
+package japicmp.output;
+
+import java.util.List;
+
+import japicmp.config.Options;
+import japicmp.model.JApiClass;
+import japicmp.output.stdout.StdoutOutputGenerator;
+
+public class StdOut extends OutputGenerator {
+
+	public StdOut(Options options, List<JApiClass> jApiClasses) {
+		super(options, jApiClasses);
+	}
+
+	@Override
+	public void generate() {
+		StdoutOutputGenerator stdoutOutputGenerator = new StdoutOutputGenerator(options);
+		String output = stdoutOutputGenerator //
+				.generate(options.getOldArchive(), options.getNewArchive(), jApiClasses);
+		System.out.println(output);
+	}
+}
