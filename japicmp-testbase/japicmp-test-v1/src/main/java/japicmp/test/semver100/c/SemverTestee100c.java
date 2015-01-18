8ebb2a8d8c229e3f1f4bdddd5f1877427f87fb17
Martin Mois <martin.mois@googlemail.com>
2015-01-18 22:10:55 +0100

    renamed further semver test cases

--- japicmp-testbase/japicmp-test-v1/src/main/java/japicmp/test/semver100/c/SemverTestee100c.java
+++ japicmp-testbase/japicmp-test-v1/src/main/java/japicmp/test/semver100/c/SemverTestee100c.java
@@ -2,19 +2,7 @@ package japicmp.test.semver100.c;
 
 public class SemverTestee100c {
 
-	public void doNothing() {
-
-	}
-
-	void doSomething() {
-
-	}
-
-	protected void doAll() {
-
-	}
-
-	private void doIt() {
+	public void methodAccessModifierChangesFromPublicToDefault() {
 
 	}
 }
--- japicmp-testbase/japicmp-test-v2/src/main/java/japicmp/test/semver100/c/SemverTestee100c.java
+++ japicmp-testbase/japicmp-test-v2/src/main/java/japicmp/test/semver100/c/SemverTestee100c.java
@@ -2,19 +2,7 @@ package japicmp.test.semver100.c;
 
 public class SemverTestee100c {
 
-	void doNothing() {
-
-	}
-
-	void doSomething() {
-
-	}
-
-	protected void doAll() {
-
-	}
-
-	private void doIt() {
+	void methodAccessModifierChangesFromPublicToDefault() {
 
 	}
 }
