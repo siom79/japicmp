1e0e2d6f263c3867451fbe4feca8f8b34ccc6c32
Martin Mois <martin.mois@googlemail.com>
2016-10-09 11:09:14 +0200

    #150 added test case for exclusion of inner package if outer package is included

--- japicmp/src/test/java/japicmp/filter/PackageFilterTest.java
+++ japicmp/src/test/java/japicmp/filter/PackageFilterTest.java
@@ -1,10 +1,18 @@
 package japicmp.filter;
 
+import japicmp.cmp.ClassesHelper;
+import japicmp.cmp.JarArchiveComparatorOptions;
+import japicmp.model.JApiClass;
 import japicmp.util.CtClassBuilder;
+import japicmp.util.CtFieldBuilder;
 import javassist.ClassPool;
 import javassist.CtClass;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static japicmp.util.Helper.getJApiClass;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -63,5 +71,31 @@ public class PackageFilterTest {
 	private CtClass createCtClassForPackage(String packageName) {
 		String className = packageName + (packageName.isEmpty() ? "" : ".") + "Test";
 		return CtClassBuilder.create().name(className).addToClassPool(new ClassPool());
+	}
+
+	@Test
+	public void testExcludeInnerPackageWhenOuterPackageIsIncluded() throws Exception {
+		JarArchiveComparatorOptions options = new JarArchiveComparatorOptions();
+		options.getFilters().getIncludes().add(new JavadocLikePackageFilter("include"));
+		options.getFilters().getExcludes().add(new JavadocLikePackageFilter("include.exclude"));
+		List<JApiClass> jApiClasses = ClassesHelper.compareClasses(options, new ClassesHelper.ClassesGenerator() {
+			@Override
+			public List<CtClass> createOldClasses(ClassPool classPool) throws Exception {
+				CtClass toInclude = CtClassBuilder.create().name("include.ToInclude").addToClassPool(classPool);
+				CtClass toExclude = CtClassBuilder.create().name("include.exclude.ToExclude").addToClassPool(classPool);
+				return Arrays.asList(toInclude, toExclude);
+			}
+
+			@Override
+			public List<CtClass> createNewClasses(ClassPool classPool) throws Exception {
+				CtClass toInclude = CtClassBuilder.create().name("include.ToInclude").addToClassPool(classPool);
+				CtClass toExclude = CtClassBuilder.create().name("include.exclude.ToExclude").addToClassPool(classPool);
+				return Arrays.asList(toInclude, toExclude);
+			}
+		});
+		assertThat(jApiClasses.size(), is(1));
+		JApiClass jApiClass = getJApiClass(jApiClasses, "include.ToInclude");
+		assertThat(jApiClass.isBinaryCompatible(), is(true));
+		assertThat(jApiClass.isSourceCompatible(), is(true));
 	}
 }
