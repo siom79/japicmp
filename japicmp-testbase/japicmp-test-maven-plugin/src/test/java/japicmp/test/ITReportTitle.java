3f01f13ab3282a1100024e6fa072a763fa0c0c09
Martin Mois <martin.mois@gmail.com>
2019-04-14 21:20:48 +0200

    upgraded version in *.md files to 0.14.0

--- japicmp-testbase/japicmp-test-maven-plugin/src/test/java/japicmp/test/ITReportTitle.java
+++ japicmp-testbase/japicmp-test-maven-plugin/src/test/java/japicmp/test/ITReportTitle.java
@@ -21,12 +21,13 @@ public class ITReportTitle {
 		assertThat(Files.exists(htmlPath), is(true));
 		Document document = Jsoup.parse(htmlPath.toFile(), "UTF-8");
 
-		Elements overviewRow = document.select("#bodyColumn tr:has([href=\"com.github.siom79.japicmp_japicmp-test-v1_0.13.2-SNAPSHOTcom.github.siom79.japicmp_japicmp-test-v2_0.13.2-SNAPSHOT_output.html\"])");
-		Elements link = overviewRow.select("[href=\"com.github.siom79.japicmp_japicmp-test-v1_0.13.2-SNAPSHOTcom.github.siom79.japicmp_japicmp-test-v2_0.13.2-SNAPSHOT_output.html\"]");
-		assertThat(link.text(), is("com.github.siom79.japicmp_japicmp-test-v1_0.13.2-SNAPSHOTcom.github.siom79.japicmp_japicmp-test-v2_0.13.2-SNAPSHOT"));
+		String projectVersion = System.getProperty("project.version");
+
+		Elements overviewRow = document.select("#bodyColumn tr:has([href=\"com.github.siom79.japicmp_japicmp-test-v1_" + projectVersion + "com.github.siom79.japicmp_japicmp-test-v2_" + projectVersion + "_output.html\"])");
+		Elements link = overviewRow.select("[href=\"com.github.siom79.japicmp_japicmp-test-v1_" + projectVersion + "com.github.siom79.japicmp_japicmp-test-v2_" + projectVersion + "_output.html\"]");
+		assertThat(link.text(), is("com.github.siom79.japicmp_japicmp-test-v1_" + projectVersion + "com.github.siom79.japicmp_japicmp-test-v2_" + projectVersion));
 
 		Elements description = overviewRow.select("td:eq(1)");
-		String projectVersion = System.getProperty("project.version");
 		assertThat(description.text(), is("Comparing source compatibility of japicmp-test-v2-"+projectVersion
 		    + ".jar against japicmp-test-v1-"+ projectVersion + ".jar"));
 	}
