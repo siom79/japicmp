229fbd913f7a261478282fd9732ac70d70af98e8
Kerscher, Michael <Michael.Kerscher@de-gmbh.com>
2021-07-19 11:45:47 +0200

    Issue #280: Test bundles added

---
+++ japicmp-testbase/japicmp-testbundle-v2/src/main/java/japicmp/testbundle/Main.java
@@ -0,0 +1,15 @@
+package japicmp.testbundle;
+
+/**
+ * TODO: Add class description!
+ *
+ * @version $Id: my.products.setup,v 1.44 2017/03/09 09:44:26 rh Exp $<br>
+ * (c) DE software &amp; control GmbH
+ */
+public class Main
+{
+    public void foo() {
+
+    }
+}
+
