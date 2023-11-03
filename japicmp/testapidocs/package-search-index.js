4baa88e5d249520be92b5838906708184ba46828
Martin Mois <martin.mois@gmail.com>
2023-11-03 20:55:51 +0100

    Initial site

---
+++ japicmp/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"japicmp"},{"l":"japicmp.cmp"},{"l":"japicmp.compat"},{"l":"japicmp.config"},{"l":"japicmp.filter"},{"l":"japicmp.output.incompatible"},{"l":"japicmp.output.semver"},{"l":"japicmp.output.stdout"},{"l":"japicmp.output.xml"},{"l":"japicmp.util"},{"l":"japicmp.versioning"}]
